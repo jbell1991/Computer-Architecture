291910960b4928e506adfa3ad19aac97ed18ffdb
jbell1991 <joseph.louis.bell@gmail.com>
2020-07-21 00:09:33 -0400

    printing 8

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,28 @@
 
 import sys
 
+# instruction codes
+HLT = 0b00000001 
+LDI = 0b10000010
+PRN = 0b01000111
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        # hold 256 bytes of memory
+        self.ram = [0] * 256
+        # hold 8 general-purpose registers
+        self.reg = [0] * 8
+        self.pc = 0
+        self.running = True
+    
+    def ram_read(self, address):
+        return self.ram[address]
+
+    def ram_write(self, value, address):
+        self.ram[address] = value
 
     def load(self):
         """Load a program into memory."""
@@ -62,4 +78,24 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        while self.running:
+            # instruction register
+            instruction_register = self.ram_read(self.pc)
+            # in case the instructions need them
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+            # perform the actions needed for instruction per the LS-8 spec
+            # halt the CPU (and exit the emulator)
+            if instruction_register == HLT:
+                self.running = False
+            # set the value of the register to an integer
+            elif instruction_register == LDI:
+                self.reg[operand_a] = operand_b
+                self.pc += 3
+            # print numeric value stored in the given register
+            elif instruction_register == PRN:
+                print(self.reg[operand_a])
+                self.pc += 2
+            else: 
+                print("Instruction not valid")
+                
